68501d7b97acd543b7c5cc1c21fe63ad99be1268
SHXBOT <ehinners@my.wctc.edu>
2020-02-04 23:25:35 -0600

    added ListServlet

--- src/edu/wctc/ListServlet.java
+++ src/edu/wctc/ListServlet.java
@@ -6,14 +6,93 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.sql.*;
 
-@WebServlet(name = "ListServlet")
+@WebServlet(name = "ListServlet", urlPatterns = "/list")
 public class ListServlet extends HttpServlet {
+    private final String DRIVER_NAME = "jdbc:derby:";
+    private final String DATABASE_PATH = "../../db";
+    private final String USERNAME = "eric";
+    private final String PASSWORD = "eric";
+
+    // Comment
+
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
     }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        // Declare outside the try/catch so the variables are in scope in the finally block
+        Connection conn = null;
+        Statement stmt = null;
+        ResultSet rset = null;
 
+        try {
+            // Load the driver
+            Class.forName("org.apache.derby.jdbc.EmbeddedDriver");
+
+            // Find the absolute path of the database folder
+            String absPath = getServletContext().getRealPath("/") + DATABASE_PATH;
+
+            StringBuilder sql = new StringBuilder("SELECT SNACK_NAME, tier");
+            sql.append(" FROM SNACKS");
+            sql.append(" ORDER BY tier"); // Don't end SQL with semicolon!
+
+            // Create a connection
+            conn = DriverManager.getConnection(DRIVER_NAME + absPath, USERNAME, PASSWORD);
+            // Create a statement to execute SQL
+            stmt = conn.createStatement();
+            // Execute a SELECT query and get a result set
+            rset = stmt.executeQuery(sql.toString());
+
+            // Create a StringBuilder for ease of appending strings
+            StringBuilder output = new StringBuilder();
+
+            // HTML to create a simple web page
+            output.append("<html><head><link href='../css/style.css' rel='stylesheet'></head><body><ul>");
+
+            // Loop while the result set has more rows
+            while (rset.next()) {
+                // Get the first string (the pet name) from each record
+                String snackName = rset.getString(1);
+                int tier = rset.getInt(2);
+                // Append it as a list item
+                output.append("<li>").append(snackName + ": " + tier).append("</li>");
+            }
+            // Close all those opening tags
+            output.append("</ul></body></html>");
+
+            // Send the HTML as the response
+            response.setContentType("text/html");
+            response.getWriter().print(output.toString());
+
+        } catch (SQLException | ClassNotFoundException e) {
+            // If there's an exception locating the driver, send IT as the response
+            response.getWriter().print(e.getMessage());
+            e.printStackTrace();
+        } finally {
+            if (rset != null) {
+                try {
+                    rset.close();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            }
+            if (stmt != null) {
+                try {
+                    stmt.close();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            }
+            if (conn != null) {
+                try {
+                    conn.close();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
     }
 }
+
