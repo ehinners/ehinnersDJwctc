f7b84858f241886b4111dccae7317ed9c943de05
SHXBOT <ehinners@my.wctc.edu>
2020-02-18 22:54:59 -0600

    listServlet2 updated

--- src/main/java/edu/wctc/SearchServlet2.java
+++ src/main/java/edu/wctc/SearchServlet2.java
@@ -7,7 +7,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-@WebServlet(name = "SearchServlet2")
+@WebServlet(name = "SearchServlet2", urlPatterns = "/search2")
 public class SearchServlet2 extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
